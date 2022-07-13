3ee1f76f5ed7541d3f32b605c1ea515446f616fa
Sevde <sevdeorscelik@gmail.com>
2022-07-13 13:40:51 +0200

    express giris

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-const http = require('http');  //istekte bulunduk
+
+const express = require('express')
 
 
+const app = express()
 
-const server = http.createServer((req, res) => {  //istekten req ve res dönüyor
-    
-    const url = req.url;
-    console.log(url);
-    
-    console.log('Bir istek gönderildi'); 
-    //res.write('merhabalar') //cevabi bu sekilde tainmladik
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World')
+})
+app.get('/about', (req, res) => {
+    res.status(200).send('about page')
+})
+app.get('/contact', (req, res) => {
+    res.status(200).send('contact page')
+})
+app.get('/contact', (req, res) => {
+    res.status(404).send('404 not found')
+})
 
-    if(url==='/'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write('<h1>merhabalar</h1>')
-    } else if(url==='/about'){
-        res.write('about sayfasidir')
-    } else if(url==='/contact'){
-        res.write('contact sayfasidir')
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        res.write('404 sayfa bulunamadi ')
-    }
+const port = 3000
 
-    res.end() //res'i bitirdik yoksa istek göndermeye devam ediyor
-});
-
-const port = 3000;
-
-server.listen(port, () => {
-    console.log(`Sunucu port ${port}'de baslatildi`);
+app.listen(port, () => {
+    console.log(`sunucu port ${port}'da calistirildi`);
 })
